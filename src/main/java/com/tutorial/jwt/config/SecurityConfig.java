db49107f4fabe6df58ba6783df29a20a34e1e30e
dongjun6343 <dongjun6343@naver.com>
2022-11-13 19:24:04 +0900

    jwt관련 생성했던 클래스 5개 SecurityConfig에 추가하기.

--- src/main/java/com/tutorial/jwt/config/SecurityConfig.java
+++ src/main/java/com/tutorial/jwt/config/SecurityConfig.java
@@ -1,18 +1,53 @@
 package com.tutorial.jwt.config;
 
 
+import com.tutorial.jwt.jwt.JwtAccessDeniedHandler;
+import com.tutorial.jwt.jwt.JwtAuthenticationEntryPoint;
+import com.tutorial.jwt.jwt.JwtSecurityConfig;
+import com.tutorial.jwt.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
 
 /**
  * jwt패키지에서 만들었던 5개의 클래스를 SecurityConfig에 추가할예정.
  *
  */
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            CorsFilter corsFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
     /**
      * /h2-console/ 하위의 모든 요청과 파비콘, /error 에 관한 것은 모두 무시하는 것으로 설정.
@@ -27,11 +62,38 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable
+                .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+
+                // Exception 핸들링 시 만들어줬던 클래스 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // h2-console 추가.
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
                 .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정한다.
                 .antMatchers("/api/hello").permitAll()
-                .anyRequest().authenticated(); // permitAll이 아닌 나머지 요청들은 모두 인증되어야 한다.
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+
+                .anyRequest().authenticated() // permitAll이 아닌 나머지 요청들은 모두 인증되어야 한다.
+
+                 .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
 
         return httpSecurity.build();
     }
-
 }
